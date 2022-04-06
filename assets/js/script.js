f6fcb435843212f8e9b5e27dec8f171b085b5026
Igor Cesar <igorcesar.dev@gmail.com>
2022-04-05 22:37:36 -0300

    supermarket-v0.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,10 +35,10 @@ function atualizarCarrinho() {
                     <p> `+ valor.quantidade + `</p>
                 </div>
                 <div class="preco-cart">
-                    <p id="valor-item">`+ valor.preco + `</p>
+                    <p>R$<span class="valor-item">`+ (valor.preco * valor.quantidade).toFixed(2) + `</span></p>
                 </div>
+                <div id="`+ valor.id + `"</div>
             </div>
-            <hr>
         `;
         }
     })
@@ -55,9 +55,43 @@ for (var i = 0; i < links.length; i++) {
     })
 }
 
-function totalCarrinho() {
-    var totalCarrinho = document.getElementById('valor-item').textContent;
+function procurarProduto() {
+    let input = document.getElementById('searchbar').value;
+    input = input.toLowerCase();
+    let item = document.getElementsByClassName('name-product');
+    let product = document.getElementsByClassName('box-product');
 
-    console.log(totalCarrinho);
+    for (i = 0; i < item.length; i++) {
+        if (!item[i].innerHTML.toLowerCase().includes(input)) {
+            product[i].style.display = "none";
+        } else {
+            product[i].style.display = "inline-block"
+        }
+    }
 }
 
+function totalCarrinho() {
+    let todosValores = document.querySelectorAll('.valor-item');
+
+    let total = 0;
+
+    for (let i = 0; i < todosValores.length; i++) {
+        let valor = Number(todosValores[i].textContent);
+        total = total + valor;
+    }
+
+
+    //
+
+       var containerTotal = document.getElementById('total-carrinho');
+       containerTotal.innerHTML = "";
+       itens.map((valor) => {
+           if (valor.quantidade > 0) {
+               containerTotal.innerHTML = `
+               <div class="preco-carrinho">
+                   <p class="valor-total">R$`+  total.toFixed(2)+ `</p>
+               </div>
+       `;
+           }
+       }) 
+}

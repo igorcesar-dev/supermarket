486c0d7d8e63403f24347eb29236931beda18f3f
igorcesar-dev <igorcesar.dev@gmail.com>
2023-03-20 20:03:38 -0300

    update_supermaket_with_vue-js

--- db/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const connection = new Sequelize('supermarket', 'root', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;

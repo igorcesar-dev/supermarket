93d967efb602cb3f7125e869119cf206efad0094
igorcesar-dev <igorcesar.dev@gmail.com>
2023-03-23 23:38:30 -0300

    modal_update

--- supermarket-ecommerce/src/main.js
+++ supermarket-ecommerce/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.js"
+import "../node_modules/bulma/css/bulma.css";
+
 
 
 createApp(App).use(router).mount("#app");
--- supermarket-ecommerce/src/views/Admin/UsersView.vue
+++ supermarket-ecommerce/src/views/Admin/UsersView.vue
@@ -1,34 +1,76 @@
 <template>
   <div>
-    <h1>painel adm</h1>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Email</th>
-          <th>Cargo</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ user.name }}</td>
-          <td>{{ user.email }}</td>
-          <td v-if="user.role == 0">Comum</td>
-          <td v-if="user.role == 1">Admin</td>
-          <td>
-            <router-link :to="{name: 'UserEdit', params: {id: user.id}}">
-              <button class="button is-success mr-2">
-                Editar
-              </button></router-link
-            >
-            <button class="button is-danger" @click="showModalUser(user.id)">
-              Excluir
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="form-create">
+      <div class="card-body">
+        <h1 class="card-title">Usuários</h1>
+        <hr />
+        <form
+          id="pesquisa"
+          action="/admin/produtos"
+          method="get"
+          class="d-flex"
+          role="search"
+        >
+          <input
+            class="form-control me-2"
+            type="search"
+            id="produto"
+            name="produto"
+            placeholder="Pesquisar usuários"
+            aria-label="Search"
+          />
+          <a
+            href="/admin/produtos"
+            class="btn btn-outline-dark"
+            style="width: 5em"
+            >Limpar</a
+          >
+          <button
+            class="btn btn-outline-success ms-2"
+            style="width: 25em"
+            type="submit"
+          >
+            Pesquisar
+          </button>
+          <a
+            class="btn btn-success ms-2"
+            style="width: 15em"
+            href="/admin/produto/novo"
+            >Novo</a
+          >
+        </form>
+      </div>
+    </div>
+    <div class="card form-create">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Cargo</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+            <td v-if="user.role == 0">Comum</td>
+            <td v-if="user.role == 1">Admin</td>
+            <td>
+              <router-link :to="{ name: 'UserEdit', params: { id: user.id } }">
+                <button class="btn btn-warning mx-2">
+                  Editar
+                </button></router-link
+              >
+              <button class="btn btn-danger" @click="showModalUser(user.id)">
+                Excluir
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
     <div :class="{ modal: true, 'is-active': showModal }">
       <div class="modal-background"></div>
@@ -61,6 +103,9 @@
       aria-label="close"
       @click="hideModal()"
     ></button>
+
+
+    
   </div>
 </template>
 
